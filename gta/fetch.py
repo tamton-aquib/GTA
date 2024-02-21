3eb410e311a407af71197b6eb32bcb4b5c9667bd
Tamton Aquib <aquibjavedt007@gmail.com>
2024-02-21 13:00:59 +0530

    fix: NoneType error when fetching article tag

--- gta/fetch.py
+++ gta/fetch.py
@@ -12,13 +12,13 @@ class Fetcher:
         boxes = soup.find_all('article')
 
         for box in boxes:
-            name = box.find('h1').find('a').text.replace('\n', '').replace(' ', '')
-            desc = box.find('p', {"class": "col-9 color-fg-muted my-1 pr-4"})
+            name = box.find('h2').find('a')
+            desc = box.find('p')
             lang = box.find('span', attrs={"itemprop": "programmingLanguage"})
             stars, forks, *_ = box.find_all('div')[2].find_all('a')
 
             self.results.append({
-                    "name": name,
+                    "name": name.text.replace('\n', '').replace(' ', '').strip(),
                     "description":  desc.text.replace('\n', '').strip() if desc else '',
                     "language": lang.text.replace('\n', '') if lang else '',
                     "stars": stars.text.replace('\n', '').strip(),
